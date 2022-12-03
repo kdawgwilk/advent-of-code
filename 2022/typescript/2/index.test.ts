cad78d00a5a25ce9ce5c1acc876e54d108bca08c
Kaden Wilkinson <7799267+kdawgwilk@users.noreply.github.com>
2022-12-02 22:59:48 -0700

    2022 Day 2

---
+++ 2022/typescript/2/index.test.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs'
+import { a, b } from '.'
+
+const sampleInput = fs.readFileSync('./2/sample.txt', 'utf8')
+const puzzleInput = fs.readFileSync('./2/input.txt', 'utf8')
+
+describe('a', () => {
+  test('sample', () => {
+    const output = a(sampleInput)
+
+    expect(output).toBe(15)
+  })
+
+  test('puzzle', () => {
+    const output = a(puzzleInput)
+
+    expect(output).toBe(14163)
+  })
+})
+
+describe('b', () => {
+  test('sample', () => {
+    const output = b(sampleInput)
+
+    expect(output).toBe(12)
+  })
+
+  test('puzzle', () => {
+    const output = b(puzzleInput)
+
+    console.log('answer: ', output)
+    expect(output).not.toBe(9539)
+    expect(output).toBeGreaterThan(9539)
+    expect(output).toBe(12091)
+  })
+})
---
+++ 2022/typescript/2/index.ts
@@ -0,0 +1,83 @@
+enum Move {
+  ROCK = 1,
+  PAPER = 2,
+  SCISSORS = 3,
+}
+
+enum RoundOutome {
+  WIN = 6,
+  DRAW = 3,
+  LOSS = 0
+}
+
+const selectScoreWithDesiredOutcome = {
+  [Move.ROCK]: {
+    [RoundOutome.DRAW]: Move.ROCK,
+    [RoundOutome.WIN]: Move.PAPER,
+    [RoundOutome.LOSS]: Move.SCISSORS,
+  },
+  [Move.PAPER]: {
+    [RoundOutome.DRAW]: Move.PAPER,
+    [RoundOutome.WIN]: Move.SCISSORS,
+    [RoundOutome.LOSS]: Move.ROCK,
+  },
+  [Move.SCISSORS]: {
+    [RoundOutome.DRAW]: Move.SCISSORS,
+    [RoundOutome.WIN]: Move.ROCK,
+    [RoundOutome.LOSS]: Move.PAPER,
+  },
+}
+
+const moveMapper = {
+  A: Move.ROCK,
+  X: Move.ROCK,
+  B: Move.PAPER,
+  Y: Move.PAPER,
+  C: Move.SCISSORS,
+  Z: Move.SCISSORS,
+}
+
+const outcomeMapper = {
+  X: RoundOutome.LOSS,
+  Y: RoundOutome.DRAW,
+  Z: RoundOutome.WIN,
+}
+
+type MoveInput = keyof typeof moveMapper
+
+function roundOutcome(opponentMove: Move, myMove: Move): RoundOutome {
+  if (opponentMove === myMove) {
+    return RoundOutome.DRAW
+  }
+
+  switch (opponentMove) {
+    case Move.ROCK:
+      return myMove === Move.PAPER ? RoundOutome.WIN : RoundOutome.LOSS
+
+    case Move.PAPER:
+      return myMove === Move.SCISSORS ? RoundOutome.WIN : RoundOutome.LOSS
+      
+    case Move.SCISSORS:
+      return myMove === Move.ROCK ? RoundOutome.WIN : RoundOutome.LOSS
+  }
+}
+
+export function a(input: string): number {
+  return input.split('\n')
+    .map(line => {
+      const [opponentMove, myMove] = line.split(' ')
+        .map((move) => moveMapper[move as MoveInput]) as [Move, Move]
+      return roundOutcome(opponentMove, myMove) + myMove
+    })
+    .reduce((acc, round) => acc + round, 0)
+}
+
+export function b(input: string): number {
+  return input.split('\n')
+    .map(line => {
+      const [opponentMoveInput, outcomeInput] = line.split(' ') as ['A' | 'B' | 'C', 'X' | 'Y' | 'Z']
+      const [opponentMove, outcome] = [moveMapper[opponentMoveInput], outcomeMapper[outcomeInput]]
+      return outcome + selectScoreWithDesiredOutcome[opponentMove][outcome]
+    })
+    .reduce((acc, round) => acc + round, 0)
+}

ff58fea66f168c499855d8974ec4662d19609756
Kaden Wilkinson <7799267+kdawgwilk@users.noreply.github.com>
2022-12-02 23:03:01 -0700

    Add puzzle template

---
+++ 2022/typescript/template/index.test.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs'
+import { a, b } from '.'
+
+const sampleInput = fs.readFileSync('./template/sample.txt', 'utf8')
+const puzzleInput = fs.readFileSync('./template/input.txt', 'utf8')
+
+describe('a', () => {
+  test('sample', () => {
+    const output = a(sampleInput)
+
+    // expect(output).toBe(1)
+  })
+
+  test('puzzle', () => {
+    const output = a(puzzleInput)
+
+    // expect(output).toBe(1)
+  })
+})
+
+describe('b', () => {
+  test('sample', () => {
+    const output = b(sampleInput)
+
+    // expect(output).toBe(1)
+  })
+
+  test('puzzle', () => {
+    const output = b(puzzleInput)
+
+    // expect(output).toBe(1)
+  })
+})
---
+++ 2022/typescript/template/index.ts
@@ -0,0 +1,8 @@
+
+export function a(input: string): number {
+  return 0
+}
+
+export function b(input: string): number {
+  return 0
+}
